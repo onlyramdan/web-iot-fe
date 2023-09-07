8669c2603ee406f1352288e5e527ee8c8eecde40
onlyramdan <onlyramdanbelajar@gmail.com>
2023-09-07 17:06:02 +0700

    report data table

--- app/controllers/dashbord_controller.rb
+++ app/controllers/dashbord_controller.rb
@@ -1,6 +1,5 @@
 class DashbordController < ApplicationController
     before_action :cek_login
     def index
-
     end
 end
--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,11 @@
 class ReportController < ApplicationController
     before_action :cek_login
     def index
+        url = '/monitoring/all'
+        @cek_data = ReportHelper.req_get(url)
+        if @cek_data['content'].present?
+            @data = @cek_data['content']
+            @meta = @cek_data['meta']
+        end
     end
 end

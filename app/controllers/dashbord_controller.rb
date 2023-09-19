99bcb07e3c00bcf2197f3c618d9e931858414ead
onlyramdan <onlyramdanbelajar@gmail.com>
2023-09-19 16:23:43 +0700

    udh bisa pake grafik

--- app/controllers/dashbord_controller.rb
+++ app/controllers/dashbord_controller.rb
@@ -1,9 +1,10 @@
 class DashbordController < ApplicationController
     before_action :cek_login
     def index
+
     end
     def get_data_mqtt
-        url = "/baca"
+        url = "baca"
         @data_mqtt = DashbordHelper.req_get(url)
         if @data_mqtt.present?
             render json: @data_mqtt
